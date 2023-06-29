1aa5edde9043b621f81b2a7c45d9258911d957f9
Daniel Vergaray <danielvergarayescobar@gmail.com>
2023-06-29 13:15:49 -0500

    prueba

--- codigo.js
+++ codigo.js
@@ -76,8 +76,6 @@ function filtrarYMostrarPorCategoria (e){
     let productosFiltrados = productos.filter(producto => producto.categoria=== e.target.value)
     creacionTarjetaProductos(productosFiltrados)
     
-    
-    
 }
 
 
