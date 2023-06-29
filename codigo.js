51f4ed78096bd9f47eedcff87e63ba4a54bbf3e5
Daniel Vergaray <danielvergarayescobar@gmail.com>
2023-06-29 11:27:00 -0500

    intento agregar eventos

--- codigo.js
+++ codigo.js
@@ -2,14 +2,14 @@ let productos = [
 
     { id: 10, nombre: "Galleta Oreo", categoria: "DULCES", precioUnitario: 5.90, linkImagen: "oreoVainilla.png" },
     { id: 15, nombre: "Bombones", categoria: "DULCES", precioUnitario: 8.50, linkImagen:"bombonesRellenos.png" },
-    { id: 20, nombre: "Doritos", categoria: "SALADOS", precioUnitario: 3.80, linkImagen: "doritos.png" },
-    { id: 25, nombre: "Inka Chips", categoria: "SALADOS", precioUnitario: 2.50, linkImagen: "inkaChips.png" },
+    { id: 20, nombre: "Doritos salados", categoria: "SALADOS", precioUnitario: 3.80, linkImagen: "doritos.png" },
+    { id: 25, nombre: "Inka Chips salados", categoria: "SALADOS", precioUnitario: 2.50, linkImagen: "inkaChips.png" },
     { id: 30, nombre: "Almendras", categoria: "SALUDABLES", precioUnitario: 10.50, linkImagen: "almendras.png" },
     { id: 35, nombre: "Nueces peladas", categoria: "SALUDABLES", precioUnitario: 12.80, linkImagen: "nuecesPeladas.png" },
 
 ]
 
-let opcionesFiltrado = ["Dulces", "Salados", "Saludables", "Todos los productos"]
+//let opcionesFiltrado = ["Dulces", "Salados", "Saludables", "Todos los productos"]
 
 //contenedorProductos.createElements
 let contenedorProductos= document.getElementById("contenedorProductos")
@@ -27,9 +27,7 @@ function creacionTarjetaProductos(arrayProductos){
         elementoCreado.innerHTML= `<h2>${cadaProducto.nombre}</h2>
         <img class="imagenesCatalogo" src="./imagenes/${cadaProducto.linkImagen}"</img>
         <h3>ONLINE:  $ ${cadaProducto.precioUnitario}</h3>
-        <div class="productos__containerBoton"><button>Agregar</button></div>
-        
-        ` 
+        <div class="productos__containerBoton"><button>Agregar</button></div>` 
         contenedorProductos.appendChild(elementoCreado)
 
     
@@ -46,7 +44,7 @@ function creacionBarraIzquierda (opcionesFiltradoArray, productos){
 
         let ulCreadoFiltos= document.createElement("ul")
         ulCreadoFiltos.innerHTML= `
-        <li class="menuFiltrado_opciones" id="botonFiltro" value=${productos.categoria} >${opcion}</li>`
+        <li class="menuFiltrado_opciones" id="botonFiltro" value=${productos.categoria}>${opcion}</li>`
         contenedorOpcionesFiltrado.appendChild(ulCreadoFiltos) 
 
          // Agrego clases
@@ -55,15 +53,33 @@ function creacionBarraIzquierda (opcionesFiltradoArray, productos){
     })
 }
 
-function filtrar (){
-    /* productosFiltrados.filtrar(producto => producto.categoria===) */
+
+let input= document.getElementById("miInput")
+let boton= document.getElementById("miBoton")
+boton.addEventListener("click", ()=> filtrarYRenderizar(productos, input.value))
+boton.addEventListener("click",saludar)
+
+function saludar(){
     alert("hola")
 }
+
+function filtrarYRenderizar(arrayProductos, valorFIltro){
+    let productosFiltrados= arrayProductos.filter(producto => producto.nombre.includes(valorFIltro))
+    creacionTarjetaProductos (productosFiltrados)
+}
+
+
+
+/* function filtrar (){
+    productosFiltrados.filtrar(producto => producto.categoria===) 
+    alert("hola")
+}
+
 
 let botonesFiltros = document.getElementsByClassName("botonFiltro")
 botonesFiltros.addEventListener("click", filtrar)
 
-/* let botonesFiltros = document.getElementsByClassName("botonFiltro")
+ let botonesFiltros = document.getElementsByClassName("botonFiltro")
 for (const botonFiltro of botonesFiltros) {
   botonFiltro.addEventListener("click", filtrarYRenderizarPorCategoria)
 }
